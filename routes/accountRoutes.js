fff9b4825e213accdbe2d3a5ce52f47b54dd54ee
Dario Junior <dario.junior.rs@gmail.com>
2021-01-24 21:19:09 -0300

    Adicionado Rotas balance, new account, depoist and withdraw

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -12,4 +12,55 @@ app.get('/accounts', async (req, res) => {
   }
 })
 
+app.post('/accounts/new', async (req, res) => {
+  try {
+    const dataNewAccount = req.body;
+    const newAccount = new accountModel(dataNewAccount);
+    await newAccount.save();
+    res.send(newAccount);
+  } catch (err) {
+    res.status(500).send("Não foi possível criar uma conta bancária, verifique: " + err);
+  }
+})
+
+app.patch('/account/deposit/:value', async (req, res) => {
+  try {
+    const value = req.params.value;
+    const ag = req.query.agencia;
+    const cc = req.query.conta;
+
+    const validate = await accountModel.countDocuments({ conta: cc })
+    if (validate == 1) {
+      await accountModel.updateOne({ agencia: ag, conta: cc }, { $inc: { balance: value } })
+      res.status(200).send('Depósito efetuado com sucesso')
+    } else {
+      res.status(404).send('Algo de errado não está certo com a transação')
+    }
+  } catch (err) {
+    res.status(500).send("Algo de errado não está certo: " + err)
+  }
+})
+
+app.patch('/account/withdraw/:value', async (req, res) => {
+  try {
+    const value = (req.params.value + 1);
+    const ag = req.query.agencia;
+    const cc = req.query.conta;
+    // console.log(req)
+    const accBalance = await accountModel.find({ conta: cc }, { _id: 0, agencia: 0, conta: 0, name: 0, __v: 0 })
+    const validateBal = accBalance[0].balance - value;
+    console.log(validateBal)
+    if (validateBal > 0) {
+      await accountModel.updateOne({ agencia: ag, conta: cc }, { $inc: { balance: value } })
+      res.status(200).send('Saque efetuado com sucesso')
+    } else {
+      res.status(404).send('Algo de errado não está certo com a transação: Saldo insuficiente')
+    }
+  } catch (err) {
+    res.status(500).send("Algo de errado não está certo: " + err)
+  }
+})
+
+
+
 export { app as accountRouter };
