bde4244e6b71169ff45bd34bda79a7350a773434
Dário Junior <dario.junior.rs@gmail.com>
2021-01-28 00:13:03 -0300

    adicionado média das contas, menores saldos, maiores saldos

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -24,7 +24,7 @@ app.post('/accounts/new', async (req, res) => {
   }
 })
 
-// Deposit méthod
+// Deposit method
 app.patch('/account/deposit/:value', async (req, res) => {
   try {
     const value = req.params.value;
@@ -43,7 +43,7 @@ app.patch('/account/deposit/:value', async (req, res) => {
   }
 })
 
-// Withdraw méthod
+// Withdraw method
 app.patch('/account/withdraw/:value', async (req, res) => {
   try {
     const tax = 1;
@@ -53,8 +53,6 @@ app.patch('/account/withdraw/:value', async (req, res) => {
     let accBalance = await accountModel.find({ conta: cc }, { _id: 0, agencia: 0, conta: 0, name: 0, __v: 0 })
     accBalance = accBalance[0].balance;
     const validateBal = accBalance - value;
-    console.log(validateBal)
-    console.log(value)
     if (validateBal >= 0) {
       await accountModel.updateOne({ agencia: ag, conta: cc }, { $set: { balance: validateBal } })
       res.status(200).send('Saque efetuado com sucesso')
@@ -66,7 +64,7 @@ app.patch('/account/withdraw/:value', async (req, res) => {
   }
 })
 
-// Check balance méthod
+// Check balance method
 app.get('/account/balance/:ag', async (req, res) => {
   try {
     const accBalance = await accountModel.find({ conta: cc }, { _id: 0, agencia: 0, conta: 0, __v: 0 })
@@ -92,5 +90,77 @@ app.delete('/account/delete/:ag/:cc', async (req, res) => {
   }
 })
 
+app.patch('/accounts/transfer/:ccDec/:ccInc/:value', async (req, res) => {
+  try {
+    const ccDec = req.params.ccDec;
+    const ccInc = req.params.ccInc;
+    let value = parseInt(req.params.value);
+    const ccOne = await accountModel.find({ conta: ccDec }, { _id: 0, __v: 0 });
+    const ccTwo = await accountModel.find({ conta: ccInc }, { _id: 0, __v: 0 });
+    const agenceAccountOne = ccOne[0].agencia;
+    const agenceAccountTwo = ccTwo[0].agencia;
+    let accuntBalanceOne = ccOne[0].balance;
+    let accountBalanceTwo = ccTwo[0].balance;
+    // console.log(accuntBalanceOne + 1, accountBalanceTwo - 1)
+    agenceAccountOne == agenceAccountTwo ? (accuntBalanceOne -= value) && (accountBalanceTwo += value) : (accuntBalanceOne -= (value + 8)) && (accountBalanceTwo + value)
+    await accountModel.bulkWrite([
+      { updateOne: { "filter": { conta: ccDec }, "update": { $set: { balance: accuntBalanceOne } } } },
+      { updateOne: { "filter": { conta: ccDec }, "update": { $set: { balance: accuntBalanceOne } } } },
+    ])
+    // console.log(accuntBalanceOne, accountBalanceTwo)
+    res.sendStatus(200);
+  } catch (err) {
+    res.send(500).status('Erro na transação, verifique os dados e tente novamente' + err)
+  }
+})
 
+app.get('/accounts/avg/:ag', async (req, res) => {
+  try {
+    const ag = req.params.ag;
+    const average = await accountModel.aggregate([
+      { $group: { _id: { agencia: "$agencia" }, balance: { $avg: "$balance" } } },
+    ])
+    let avg = 0;
+    avg = average.find((agency) => {
+      let avgValue;
+      agency._id.agencia == 99 ? avgValue = agency.balance : '';
+      return avgValue
+    })
+    res.status(200).send(`Agência: ${ag} Média de Saldo: ${avg.balance}`);
+  } catch (err) {
+    res.status(500).send('Houve um erro na obtenção das médias, cheque as informações: ' + err)
+  }
+})
+
+app.get('/accounts/pobres/:limit', async (req, res) => {
+  try {
+    const limit = parseInt(req.params.limit);
+    const data = await accountModel.find({}, { _id: 0, name: 0, __v: 0 }).limit(limit).sort({ balance: 1 })
+    res.send(data)
+  } catch (err) {
+    res.status(500).send('Houve um erro na listagem, por favor cheque as informações: ' + err)
+  }
+})
+
+app.get('/accounts/ricos/:limit', async (req, res) => {
+  try {
+    const limit = parseInt(req.params.limit);
+    const data = await accountModel.find({}, { _id: 0, __v: 0 }).limit(limit).sort({ balance: -1 })
+    res.send(data)
+  } catch (err) {
+    res.status(500).send('Houve um erro na listagem, por favor cheque as informações: ' + err)
+  }
+})
+
+app.get('/accounts/privates', async (req, res) => {
+  try {
+
+    const data = await accountModel.aggregate([
+      { $group: { _id: { agencia: "$agencia", name: "$name" } } },
+    ]).sort({ balance: 1 }).limit(1)
+    res.send(data)
+  } catch (err) {
+    res.status(500).send('Houve um erro na listagem, por favor cheque as informações: ' + err)
+  }
+})
 export { app as accountRouter };
